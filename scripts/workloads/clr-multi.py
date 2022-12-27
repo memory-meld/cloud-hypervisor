2935993fda726b4a2005f53ad313ed2b4331590f
Junliang HU <jlhu@cse.cuhk.edu.hk>
2022-12-27 23:42:37 +0800

    scripts: support multi VM redis workload

--- scripts/workloads/clr-multi.py
+++ scripts/workloads/clr-multi.py
@@ -2,9 +2,10 @@
 import argparse
 import time
 from contextlib import ExitStack, contextmanager
+from functools import reduce
 from pathlib import Path
-from subprocess import Popen, run, check_output
-from typing import List
+from subprocess import Popen, check_output, run
+from typing import List, Tuple
 
 from numa.info import node_to_cpus
 
@@ -35,11 +36,13 @@ DEFAULT_CMDLINE = " ".join(
     ]
 )
 
+# this dir is mapped to the guest for easy access to supplementary data
 PROJECT_DIR = "/home/jlhu/Projects"
 DEFAULT_TEST_DIR = PROJECT_DIR + "/ch-test"
 CLOUD_HYPERVISOR = DEFAULT_TEST_DIR + "/base/cloud-hypervisor"
 VIRTIOFSD = DEFAULT_TEST_DIR + "/base/virtiofsd"
 CH_REMOTE = DEFAULT_TEST_DIR + "/base/ch-remote"
+GO_YCSB = DEFAULT_TEST_DIR + "/base/go-ycsb"
 DEFAULT_NETWORK_CONFIG = """
     <network>
       <name>default</name>
@@ -52,6 +55,29 @@ DEFAULT_NETWORK_CONFIG = """
       </ip>
     </network>
 """
+YCSB_RECORD_COUNT = 3000000
+YCSB_OPERATION_COUNT = 10000000
+YCSB_PRELOADED = f"ycsb-{YCSB_RECORD_COUNT}.rdb"
+YCSB_A_ARGS = [
+    "-p",
+    "workload=core",
+    "-p",
+    "readallfields=true",
+    "-p",
+    "readproportion=0.5",
+    "-p",
+    "updateproportion=0.5",
+    "-p",
+    "scanproportion=0",
+    "-p",
+    "insertproportion=0",
+    "-p",
+    "requestdistribution=uniform",
+]
+
+
+def get_cwd(id: int) -> str:
+    return DEFAULT_TEST_DIR + f"/vm{id}"
 
 
 def get_ip(id: int) -> str:
@@ -130,7 +156,7 @@ def create_vm(id: int, ncpus: int = 4, mem: int = 8 << 30):
     To use this on a list of VMs, see
     [`contextlib.ExitStack()`](https://stackoverflow.com/a/45681273).
     """
-    cwd = DEFAULT_TEST_DIR + f"/vm{id}"
+    cwd = get_cwd(id)
     host_cpus = ",".join(map(str, node_to_cpus(0)))
     affinity = ",".join([f"{i}@[{host_cpus}]" for i in range(ncpus)])
     mac = get_mac(id)
@@ -193,15 +219,90 @@ def ssh_cmd(id: int, args: List[str], **kwargs) -> str:
     return check_output(["ssh", get_ip(id)] + args, **kwargs).decode("utf-8")
 
 
+def subcmd_redis(args, vms: List[Popen]):
+    server_args = [
+        "tmux",
+        "new",
+        "-d",
+        "redis-server",
+        "--save",
+        "",
+        "--appendonly",
+        "no",
+        "--protected-mode",
+        "no",
+        "--dbfilename",
+        YCSB_PRELOADED,
+        "--dir",
+        DEFAULT_TEST_DIR,
+    ]
+    for id in range(args.num):
+        # launch redis with preloaded ycsb keys in the background
+        ssh_cmd(id, server_args)
+    # wait for boot
+    # wait for loading
+    while not reduce(
+        bool.__and__,
+        map(
+            lambda i: ssh_cmd(i, ["redis-cli", "dbsize"]).strip()
+            == f"{YCSB_RECORD_COUNT}",
+            range(args.num),
+        ),
+    ):
+        time.sleep(5)
+    # run ycsb on node 1 to prevent interference with VM running on node 0
+    ycsb_a_args = [
+        "numactl",
+        "--cpunodebind=1",
+        "--membind=1",
+        "--",
+        GO_YCSB,
+        "run",
+        "redis",
+        "-p",
+        f"recordcount={YCSB_RECORD_COUNT}",
+        "-p",
+        f"operationcount={YCSB_OPERATION_COUNT}",
+        "-p",
+        f"threadcount={args.ncpus}",
+    ] + YCSB_A_ARGS
+    jobs = [
+        Popen(ycsb_a_args + ["-p", f"redis.addr={get_ip(id)}:6379"], cwd=get_cwd(id))
+        for id in range(args.num)
+    ]
+    for i, (out, err) in enumerate(wait_for_exit(jobs)):
+        print(f"vm{i} stdout:")
+        print(out)
+        print(f"vm{i} stderr:")
+        print(err)
+
+
+# return (stdout, stderr) of all subprocesses
+def wait_for_exit(subprocesses: List[Popen]) -> List[Tuple[str, str]]:
+    """Call communicate() and return str (stdout, stderr) for each process."""
+    return [
+        (out.decode("utf-8"), err.decode("utf-8"))
+        for (out, err) in map(Popen.communicate, subprocesses)
+    ]
+
+
+def wait_for_boot(num: int):
+    for i in range(num):
+        check_output(f"until ssh {get_ip(i)} uname -a; do sleep 1; done", shell=True)
+
+
 def main(args):
     with pmem(), network(args.num), ExitStack() as stack:
         vms = [
             stack.enter_context(create_vm(i, args.ncpus, args.mem))
             for i in range(args.num)
         ]
+        wait_for_boot(args.num)
         match args.subcmd:
             case "manual":
-                [vm.communicate() for vm in vms]
+                wait_for_exit(vms)
+            case "redis":
+                subcmd_redis(args, vms)
             case _:
                 print(f"not implemented subcmd: {args.subcmd}")
 
@@ -229,7 +330,7 @@ if __name__ == "__main__":
     subcmd = parser.add_subparsers(
         dest="subcmd", title="workloads", description="valid workloads"
     )
-    redis = subcmd.add_parser("redis")
-    mix = subcmd.add_parser("mix")
-    manual = subcmd.add_parser("manual")
+    redis_parser = subcmd.add_parser("redis")
+    mix_parser = subcmd.add_parser("mix")
+    manual_parser = subcmd.add_parser("manual")
     main(parser.parse_args())
