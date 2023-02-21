466852dd1d3510f318ae3a04277c2ea13d3199ca
Junliang HU <jlhu@cse.cuhk.edu.hk>
2023-02-21 10:07:55 +0800

    scripts: update

--- scripts/batch.py
+++ scripts/batch.py
@@ -1,4 +1,4 @@
-from contextlib import redirect_stdout, redirect_stderr
+from contextlib import contextmanager, redirect_stdout, redirect_stderr
 from logging import info
 from pathlib import Path
 from datetime import datetime
@@ -7,40 +7,50 @@ from subprocess import check_output
 
 from rich import inspect, print
 
-from bencher import main, Opt, Benchmark, ENV_SETUP_SCRIPTS
+from bencher import main, Args, Bench, ENV_SETUP_SCRIPTS
 
 
-def outdir(name: str = str(datetime.now().isoformat())) -> Path:
+@contextmanager
+def outdir(name: str = str(datetime.now().isoformat())):
     script_dir = Path(__file__).resolve().parent
     d = script_dir / name
-    d.mkdir(parents=True, exist_ok=True)
-    return d
+    try:
+        d.mkdir(parents=True, exist_ok=True)
+        yield d
+    finally:
+        # cleanup if nothing has been generated
+        try:
+            dir.rmdir()
+        except OSError:
+            pass
 
 
 if __name__ == "__main__":
     skip = []  # ["freq"]
-    for name, script in ENV_SETUP_SCRIPTS:
+    for name, script in ENV_SETUP_SCRIPTS.items():
         if name in skip:
             continue
         check_output(script, shell=True)
 
-    dir = outdir()
-    for num, ratio in product(range(1, 28), [0.2, 0.625]):
-        opt = Opt.from_dict(
-            dict(
-                bench=Benchmark.REDIS,
-                num=num,
-                dram_ratio=ratio,
-                perf="r80D1:P",
-                pretty=True,
+    with outdir() as dir:
+        # for num, ratio in product([1, 2, 8, 9, 10, 17, 18, 19], [0.2, 0.625]):
+        for num, ratio in product([], [0.2, 0.625]):
+            opt = Args().from_dict(
+                dict(
+                    bench=Bench.REDIS,
+                    num=num,
+                    dram_ratio=ratio,
+                    # perf_event="r80D1:P",
+                    # perf_event="MEM_TRANS_RETIRED.LOAD_LATENCY_GT_256:P",
+                    pretty=True,
+                )
             )
-        )
-        inspect(opt)
-        log = dir / f"redis-ycsb-a-{num}-4-8-{ratio:.3f}.log"
-        try:
-            # , redirect_stderr(f)
-            with log.open("w") as f, redirect_stdout(f):
-                main(opt)
-        except Exception:
-            print(f"failed case: config num {num} dram ratio {ratio}")
-            log.unlink()
+            print(opt)
+            log = dir / f"redis-ycsb-a-{num}-4-8-{ratio:.3f}.log"
+            try:
+                # logging will cache fd which will cause redirect_stderr(f) to misbehave
+                with log.open("w") as f, redirect_stdout(f):
+                    main(opt)
+            except Exception:
+                print(f"failed case: config num {num} dram ratio {ratio}")
+                log.unlink()
---
+++ scripts/parse.py
@@ -0,0 +1,34 @@
+from tap import Tap
+from pathlib import Path
+import re
+from rich import print
+
+
+class Args(Tap):
+    file: Path  # file to be processed
+
+
+def parse_gap_bc(log: str) -> list[list[float]]:
+    vm = re.compile(r"(?P<tt>^vm(?P<id>\d+) gap_bc.*^Average Time:.+$)", re.S | re.M)
+    trial = re.compile(r"Trial Time: \s+([\d\.]+)")
+    return [[float(time) for time in trial.findall(txt)] for txt, _ in vm.findall(log)]
+
+
+def parse_dmesg(log: str) -> list[list[float]]:
+    vm = re.compile(r"(?P<tt>^vm(?P<id>\d+) dmesg.*?^\[\s*[\.\d]+\].*$)", re.S | re.M)
+    trial = re.compile(r"Trial Time: \s+([\d\.]+)")
+    return [[float(time) for time in trial.findall(txt)] for txt, _ in vm.findall(log)]
+
+
+def main(args: Args):
+    print(f"processing {args.file}")
+
+    with open(args.file, "r") as f:
+        content = f.read()
+        trial_time = parse_gap_bc(content)
+        print(trial_time)
+
+
+if __name__ == "__main__":
+    args = Args().parse_args()
+    main(args)
